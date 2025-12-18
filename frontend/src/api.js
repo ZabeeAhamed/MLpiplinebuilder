3e18abe9b66b0ab272c69fefe69763c4d86a4e12
zabeeahamed628@gmail.com <zabeeahamed628@gmail.com>
2025-12-18 11:49:49 +0530

    Added dockerfile for backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8000",
 });
 
 export const uploadFile = (file) => {
